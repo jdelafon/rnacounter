5e58139bc9644494570c821d4b59a288288f8b7c
Julien Delafontaine <julien.delafontaine@epfl.ch>
2014-05-20 14:38:13 +0200

    tighter doc

--- rnacounter_main.py
+++ rnacounter_main.py
@@ -11,16 +11,15 @@ Usage:
                [--version] [-h]
 
 Options:
-   -t TYPE, --type TYPE                 Type of genomic feature to count on: 'genes' or 'transcripts'
-                                        [default: genes].
-   -n <int>, --normalize <int>          Normalization constant. Default: (total number of mapped reads)/10^6.
-   -l <int>, --fraglength <int>         Average fragment length [default: 350].
-   -s, --stranded                       Compute sense and antisense reads separately [default: False].
-   -m, --multiple                       Divide count by NH flag for multiply mapping reads [default: False].
-   -c CHROMS, --chromosomes CHROMS      Chromosome names (comma-separated list).
-   -o OUTPUT, --output OUTPUT           Output file to redirect stdout.
-   -v, --version                        Displays version information and exits.
-   -h, --help                           Displays usage information and exits.
+   -t TYPE, --type TYPE             Type of genomic feature to count on: 'genes' or 'transcripts' [default: genes].
+   -n <int>, --normalize <int>      Normalization constant. Default: (total number of mapped reads)/10^6.
+   -l <int>, --fraglength <int>     Average fragment length [default: 350].
+   -s, --stranded                   Compute sense and antisense reads separately [default: False].
+   -m, --multiple                   Divide count by NH flag for multiply mapping reads [default: False].
+   -c CHROMS, --chromosomes CHROMS  Chromosome names (comma-separated list).
+   -o OUTPUT, --output OUTPUT       Output file to redirect stdout.
+   -v, --version                    Displays version information and exits.
+   -h, --help                       Displays usage information and exits.
 """
 
 from rnacounter import rnacounter_main
